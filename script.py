365d67131b934ee5136f38e8389981fb9e556f07
chaya2z <itunano57@gmail.com>
2020-03-29 23:59:33 +0900

    add try. If status-code is not 200, while loop tries again.

--- script.py
+++ script.py
@@ -1,9 +1,10 @@
 import requests
 from bs4 import BeautifulSoup
 import time
+from requests.exceptions import Timeout
 
 # header
-headers = {"User-Agent": "Mozilla/5.0 (X11; Linux x86_64; rv:61.0) Gecko/20100101 Firefox/61.0"}
+headers = {"User-Agent": ""}
 
 # To load index
 primary_link = "https://it-words.jp/"
@@ -14,23 +15,25 @@ def main():
     if start_script == "" or start_script == "y" or start_script == "Y":
         all_target_URLs = load_index_urls(convert_html(primary_link))
         for index_url in all_target_URLs:
-            # A,B,C...9 のワードのリンク一覧を取得
-            # index_url = https://it-words.jp/p/i-a.html
             print(index_url)
-            words_url = load_words_title(index_url)  # https://A-AUTO, A2DP, ...
+            words_url = load_words_title(index_url)
             for a_word in words_url:
+                time.sleep(10)
                 print(load_words_details(a_word))
-                time.sleep(3)
-
     else:
         print("Bye")
 
 
 def convert_html(target_url):
-    r = requests.get(url=target_url, headers=headers)
-    r.raise_for_status()  # Check status-code response from server. If not 200, stop scraping.
-    soup = BeautifulSoup(r.content, "html.parser")
-    return soup
+    while True:
+        try:
+            r = requests.get(url=target_url, headers=headers, timeout=30)
+            r.raise_for_status()  # Check status-code response from server. If not 200, stop scraping.
+            soup = BeautifulSoup(r.content, "html.parser")
+            return soup
+            break
+        except Timeout:
+            convert_html(target_url)
 
 
 def load_index_urls(target_html):
@@ -40,7 +43,7 @@ def load_index_urls(target_html):
     for i in result:
         wordURL = i.get("href")
         targetURLs.append(wordURL)
-        # time.sleep(5)
+
     return targetURLs
 
 
@@ -51,18 +54,18 @@ def load_words_title(target_url):
     terms_table = soup.find("table", class_="term").select("a")
     for i in terms_table:
         target_urls.append(i.get("href"))
-        # time.sleep(5)
+
     return target_urls
 
 
-def load_words_details(target_url):  # https://...A-AUTO... .html
+def load_words_details(target_url):
     soup = convert_html(target_url)
 
     term_title = soup.h1.string
     print(term_title)
 
-    # print(soup.find("div", class_="entryBody")) #  <p>...</p>
     targetWordExplanation = soup.find("div", class_="entryBody").text
+
     return targetWordExplanation
 
 
